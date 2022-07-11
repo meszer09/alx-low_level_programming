3022e7328b164bafa74e6f4b6d745a830fe365c9
mesfin zerihun <mesfinzerihun1@gmail.com>
2022-07-11 12:11:32 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unisted.h>
 
 /**
 * main - prints exactly "and that piece of art is useful"
@@ -10,4 +9,5 @@
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
